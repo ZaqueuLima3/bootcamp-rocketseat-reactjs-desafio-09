bcb62a07ef4668536a0dc4ee0ba08587414cbe7d
zaqueu lima dos santos <zaqueulima1@gmail.com>
2019-08-03 12:37:58 -0300

    Initial routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
-import Header from '~/components/Header';
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </>
+  );
 }
 
 export default App;
